b6049cbd58c0654aa3eb724fa73badee1208b97c
makambu <makambuortho2@gmail.com>
2025-06-30 18:12:04 +0100

    🛠️ Ajout script de démarrage Render avec collectstatic et Daphne

--- render_start.sh
+++ render_start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Lire le port depuis l'environnement (Render)
-export PORT=${PORT:-8000}
-
-# Lancer Daphne sur le bon port
-exec daphne -b 0.0.0.0 -p $PORT UserCompte.asgi:application
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+daphne -b 0.0.0.0 -p $PORT UserCompte.asgi:application
