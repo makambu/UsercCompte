d652301f41f534ce643a35cf3b6fb52dd3d4d011
makambu <makambuortho2@gmail.com>
2025-07-03 14:44:17 +0100

    🔧 Fix WebSocket + sonnerie + badge + asgi.py

--- UserCompte/asgi.py
+++ UserCompte/asgi.py
@@ -1,33 +1,22 @@
-"""
-ASGI config for UserCompte project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UserCompte.settings')
 django.setup()
-application = get_asgi_application()
 
-
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.sessions import SessionMiddlewareStack
 import app_usercompte.chat.routing
-
-
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            app_usercompte.chat.routing.websocket_urlpatterns
+    "websocket": SessionMiddlewareStack(
+        AuthMiddlewareStack(
+            URLRouter(
+                app_usercompte.chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
-
