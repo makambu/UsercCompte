c38cd4a94ca20af45d61e390a78d422e06a3e3fc
makambu <makambuortho2@gmail.com>
2025-07-05 10:11:05 +0100

    fix: normalize line endings LF

--- render_start.sh
+++ render_start.sh
@@ -3,11 +3,8 @@
 echo "📦 Installation des dépendances..."
 pip install -r requirements.txt
 
-echo "🔧 Application des migrations..."
-python manage.py migrate --noinput
-
 echo "📦 Collecte des fichiers statiques..."
 python manage.py collectstatic --noinput
 
-echo "🚀 Lancement de Daphne..."
+python manage.py migrate --noinput
 daphne -b 0.0.0.0 -p $PORT UserCompte.asgi:application
